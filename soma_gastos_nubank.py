3be9efb304b155dfb32937dc5b3efdaf39d4202a
Alan Rios <35049559+hirios@users.noreply.github.com>
2023-06-19 19:20:40 -0300

    Update soma_gastos_nubank.py

--- soma_gastos_nubank.py
+++ soma_gastos_nubank.py
@@ -1,10 +1,43 @@
-
-function GASTOS(nuJson) {
-    nuJson.bill.line_items.forEach((e) => {
-        console.log(e.post_date + '|' + e.title + '|' + e.amount.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}))
-    })
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+    document.body.removeChild(element);
 }
 
-GASTOS()
+function toBR(numero) {
+    let out = (numero / 100).toString()
+    return out
+}
+
+function GASTOS(nuJson) {
+    let csvSting = ''
+    nuJson.bill.line_items.forEach((e) => {
+        csvSting += e.post_date + '|' + e.title + '|' + toBR(e.amount) + '\n'
+    })
+
+    download('text.txt', csvSting)
+}
+
+var header = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": JSON.parse(sessionStorage.getItem('token__pf')).token_type + ' ' + JSON.parse(sessionStorage.getItem('token__pf')).access_token
+    }
+  }
+
+async function logJSONData() {
+    let url = JSON.parse(sessionStorage.getItem('discovery_endpoints__bills')).open
+    const response = await fetch(url, header);
+    const jsonData = await response.json();
+    return jsonData
+  }
 
 
+GASTOS(await logJSONData())
